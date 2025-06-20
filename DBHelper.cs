ada9fb7f357f4fe2a3c6b664255ba3482e3aad02
Thiranjshan <thiranjayaishan@gmail.com>
2025-06-20 23:06:28 +0530

    db connected

---
+++ DBHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Inventory_manager
+{
+    internal class DBHelper
+    {
+        private static readonly string connectionString = "Data Source=inventoryDB.db;Version=3;";
+        public static SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+        public static void CreateDatabaseIfNotExists()
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER)", connection);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+using System.Data.SQLite;
 namespace Inventory_manager
 {
 
@@ -10,6 +13,18 @@ namespace Inventory_manager
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            try
+            {
+                using (var conn = DBHelper.GetConnection())
+                {
+                    conn.Open();
+                    MessageBox.Show("Connected to SQLite DB successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to connect: " + ex.Message);
+            }
 
         }
     }
